a6fd1ea491dde3a3fdcb858b22af52b071d89f23
Alex <alexchelseaalex@gmail.com>
2022-07-15 01:53:26 +0100

    update contract to use dotenv variables

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,25 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@typechain/hardhat'
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-ethers"
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      // @ts-ignore
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
